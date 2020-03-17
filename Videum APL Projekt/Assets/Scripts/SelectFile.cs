2f6b0e44a5a6789633ea10dba0381d9f5ee7a1d4
SchanicJB <62284245+SchanicJB@users.noreply.github.com>
2020-03-17 15:05:41 +0100

    Start

---
+++ Videum APL Projekt/Assets/Scripts/SelectFile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectFile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+}
